05651f716f8273d2f85f8d8ac7d72744400c056a
ningningzhang-nina <127649732+ningningzhang-nina@users.noreply.github.com>
2024-03-20 16:41:49 +0800

    Delete application directory

--- application/partial_linear_regression.R
+++
@@ -1,9 +0,0 @@
-partial_linear_regression<-function(X,Y,Z,h1,h2){
-  source("kernel_regression_nw.R")
-  gy_hat<-mNW(Z, Z, Y, h1, K = dnorm)
-  gx_hat<-mNW(Z, Z, X, h2, K = dnorm)
-  error1<-Y-gy_hat
-  error2<-X-gx_hat
-  beta_hat<-(sum(error2^2))^(-1)*sum(error2*error1)
-  return(beta_hat)
-}
