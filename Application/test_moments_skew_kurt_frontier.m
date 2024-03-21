5d1f91ec9e8b238130096d1fa498bef0644a28fb
ningningzhang-nina <127649732+ningningzhang-nina@users.noreply.github.com>
2024-03-21 09:36:21 +0800

    Update test_moments_skew_kurt_frontier.m

--- Application/test_moments_skew_kurt_frontier.m
+++ Application/test_moments_skew_kurt_frontier.m
@@ -1,7 +1,7 @@
 clear all
 index_names = {'AUD_USD','NZD_USD','CAD_USD'};
 for num =1:3
-    load(sprintf("final_%s_moments.mat",index_names{num}))
+    load(sprintf("./results/final_%s_moments.mat",index_names{num}))
     sigma0 = moments(1,:);
     skew0 = moments(2,:);
     kurt0 = moments(3,:);
