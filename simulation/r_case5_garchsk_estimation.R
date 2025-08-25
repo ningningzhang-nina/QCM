9c482096818905e139162d63786cf94d20878569
ningningzhang-nina <127649732+ningningzhang-nina@users.noreply.github.com>
2025-08-25 08:46:39 +0800

    Add files via upload

--- simulation/r_case5_garchsk_estimation.R
+++ simulation/r_case5_garchsk_estimation.R
@@ -1,7 +1,7 @@
 library("R.matlab")
 library("GARCHSK")
 library("MASS")
-setwd("/Users/gina/Downloads/Research/2.QCM/Code files/final_simulation/results/")
+setwd("/Users/gina/Downloads/Research/2.QCM/Code files/final_simulation")
 #######CASE 1
 #xdata<-readMat("new_simulation_garch.mat")
 xdata<-readMat("new_DGP1_garch.mat")
@@ -21,7 +21,24 @@ write.matrix(muhat,file="new_dgp1_case5_muhat.csv")
 write.matrix(sigma2hat,file="new_dgp1_case5_sigma2hat.csv")
 write.matrix(skewhat,file="new_dgp1_case5_skewhat.csv")
 write.matrix(kurthat,file="new_dgp1_case5_kurthat.csv")
-
+####正在跑
+xdata<-readMat("no_variance_new_DGP1_garch.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$yy[rep,])
+  results<-garchsk_construct(Est$params,xdata$yy[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="no_variance_new_dgp1_case5_muhat.csv")
+write.matrix(sigma2hat,file="no_variance_new_dgp1_case5_sigma2hat.csv")
+write.matrix(skewhat,file="no_variance_new_dgp1_case5_skewhat.csv")
+write.matrix(kurthat,file="no_variance_new_dgp1_case5_kurthat.csv")
 #######CASE 2
 #xdata<-readMat("final_simulation_studentt_timevaring_original_noar.mat")
 xdata<-readMat("new_DGP2_studentt.mat")
@@ -41,6 +58,65 @@ write.matrix(muhat,file="new_dgp2_case5_muhat.csv")
 write.matrix(sigma2hat,file="new_dgp2_case5_sigma2hat.csv")
 write.matrix(skewhat,file="new_dgp2_case5_skewhat.csv")
 write.matrix(kurthat,file="new_dgp2_case5_kurthat.csv")
+#######CASE 22
+#xdata<-readMat("final_simulation_studentt_timevaring_original_noar.mat")
+xdata<-readMat("new_new_DGP2_studentt.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$y[rep,])
+  results<-garchsk_construct(Est$params,xdata$y[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="new_new_dgp2_case5_muhat.csv")
+write.matrix(sigma2hat,file="new_new_dgp2_case5_sigma2hat.csv")
+write.matrix(skewhat,file="new_new_dgp2_case5_skewhat.csv")
+write.matrix(kurthat,file="new_new_dgp2_case5_kurthat.csv")
+#######CASE 222
+#xdata<-readMat("final_simulation_studentt_timevaring_original_noar.mat")
+xdata<-readMat("no_variance_new_new_DGP2_studentt.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$y[rep,])
+  results<-garchsk_construct(Est$params,xdata$y[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="no_variance_new_new_dgp2_case5_muhat.csv")
+write.matrix(sigma2hat,file="no_variance_new_new_dgp2_case5_sigma2hat.csv")
+write.matrix(skewhat,file="no_variance_new_new_dgp2_case5_skewhat.csv")
+write.matrix(kurthat,file="no_variance_new_new_dgp2_case5_kurthat.csv")
+
+
+
+## chisquare
+xdata<-readMat("new_DGP6_chisquare.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$y[rep,])
+  results<-garchsk_construct(Est$params,xdata$y[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="dgp6_case5_muhat.csv")
+write.matrix(sigma2hat,file="dgp6_case5_sigma2hat.csv")
+write.matrix(skewhat,file="dgp6_case5_skewhat.csv")
+write.matrix(kurthat,file="dgp6_case5_kurthat.csv")
 #######CASE 3
 #xdata<-readMat("simulation_garch_snp_caviar.mat")
 xdata<-readMat("new_DGP3_garch_snp.mat")
@@ -60,6 +136,25 @@ write.matrix(muhat,file="new_dgp3_case5_muhat.csv")
 write.matrix(sigma2hat,file="new_dgp3_case5_sigma2hat.csv")
 write.matrix(skewhat,file="new_dgp3_case5_skewhat.csv")
 write.matrix(kurthat,file="new_dgp3_case5_kurthat.csv")
+#xdata<-readMat("simulation_garch_snp_caviar.mat")
+#####
+xdata<-readMat("no_variance_new_DGP3_garch_snp.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$yy[rep,])
+  results<-garchsk_construct(Est$params,xdata$yy[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="no_variance_new_dgp3_case5_muhat.csv")
+write.matrix(sigma2hat,file="no_variance_new_dgp3_case5_sigma2hat.csv")
+write.matrix(skewhat,file="no_variance_new_dgp3_case5_skewhat.csv")
+write.matrix(kurthat,file="no_variance_new_dgp3_case5_kurthat.csv")
 #######CASE 4
 xdata<-readMat("simulation_gjr_skew_studentt_caviar.mat")
 muhat<-matrix(0,100,1000)
@@ -96,6 +191,24 @@ write.matrix(muhat,file="new_dgp4_case5_muhat.csv")
 write.matrix(sigma2hat,file="new_dgp4_case5_sigma2hat.csv")
 write.matrix(skewhat,file="new_dgp4_case5_skewhat.csv")
 write.matrix(kurthat,file="new_dgp4_case5_kurthat.csv")
+#######
+xdata<-readMat("no_variance_new_DGP4_gir_skew_studentt.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$yyy[rep,])
+  results<-garchsk_construct(Est$params,xdata$yyy[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="no_variance_new_dgp4_case5_muhat.csv")
+write.matrix(sigma2hat,file="no_variance_new_dgp4_case5_sigma2hat.csv")
+write.matrix(skewhat,file="no_variance_new_dgp4_case5_skewhat.csv")
+write.matrix(kurthat,file="no_variance_new_dgp4_case5_kurthat.csv")
 #######CASE 5
 xdata<-readMat("new_simulation_engle.mat")
 muhat<-matrix(0,100,1000)
@@ -114,3 +227,22 @@ write.matrix(muhat,file="dgp5_case5_muhat.csv")
 write.matrix(sigma2hat,file="dgp5_case5_sigma2hat.csv")
 write.matrix(skewhat,file="dgp5_case5_skewhat.csv")
 write.matrix(kurthat,file="dgp5_case5_kurthat.csv")
+
+#######CASE 5
+xdata<-readMat("no_variance_dgp5_arma_mx_garch.mat")
+muhat<-matrix(0,100,1000)
+sigma2hat<-matrix(0,100,1000)
+skewhat<-matrix(0,100,1000)
+kurthat<-matrix(0,100,1000)
+for (rep in 1:100){
+  Est<-garchsk_est(xdata$yy[rep,])
+  results<-garchsk_construct(Est$params,xdata$yy[rep,])
+  muhat[rep,]<-results$mu
+  sigma2hat[rep,]<-results$h
+  skewhat[rep,]<-results$sk
+  kurthat[rep,]<-results$ku
+}
+write.matrix(muhat,file="no_variance_dgp5_case5_muhat.csv")
+write.matrix(sigma2hat,file="no_variance_dgp5_case5_sigma2hat.csv")
+write.matrix(skewhat,file="no_variance_dgp5_case5_skewhat.csv")
+write.matrix(kurthat,file="no_variance_dgp5_case5_kurthat.csv")
